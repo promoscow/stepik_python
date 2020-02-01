9260c283b5b34e06374d8d68dadfb1ca41869cb2
Vyacheslav Chernyshoff <slava_rossii@list.ru>
2020-02-01 20:47:03 +0300

    * 90 очков

--- stepik_ex_3.py
+++ stepik_ex_3.py
@@ -1,2 +1,19 @@
+import requests
+
+root_url = 'https://stepic.org/media/attachments/course67/3.6.3/'
+with open('dataset_3378_3.txt', encoding='utf8') as inf:
+    url = inf.readline().strip()
+
+response = requests.get(url)
+
+while True:
+    text = response.text
+    print(text)
+    if text.splitlines()[0].split(' ')[0] == 'We':
+        break
+    url = root_url + text
+    response = requests.get(url)
+print("----------")
+print(text)
 
 
